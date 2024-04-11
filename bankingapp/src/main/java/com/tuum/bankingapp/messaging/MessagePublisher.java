bd6daf0d132fc498a988daa9a0e7a9717153e183
Ralf Animägi <116784913+Ralf-A@users.noreply.github.com>
2024-04-11 04:12:59 +0300

    RabbitMQ Message publishing

--- bankingapp/src/main/java/com/tuum/bankingapp/messaging/MessagePublisher.java
+++ bankingapp/src/main/java/com/tuum/bankingapp/messaging/MessagePublisher.java
@@ -2,6 +2,8 @@ package com.tuum.bankingapp.messaging;
 
 import com.tuum.bankingapp.model.Account;
 import com.tuum.bankingapp.model.Transaction;
+import org.slf4j.Logger;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MessagePublisher {
+    Logger log = org.slf4j.LoggerFactory.getLogger(MessagePublisher.class);
 
     private final RabbitTemplate rabbitTemplate;
 
@@ -18,11 +21,22 @@ public class MessagePublisher {
     }
 
     public void publishAccountEvent(Account event) {
-        rabbitTemplate.convertAndSend("bankingExchange", "account.created", event);
+        try {
+            rabbitTemplate.convertAndSend("bankingExchange", "account.created", event);
+            log.info("Published account creation event: {}", event);
+        } catch (AmqpException e) {
+            log.error("Failed to publish account creation event", e);
+        }
     }
 
+
     public void publishTransactionEvent(Transaction event) {
-        rabbitTemplate.convertAndSend("bankingExchange", "transaction.created", event);
+        try{
+            rabbitTemplate.convertAndSend("bankingExchange", "transaction.created", event);
+            log.info("Published transaction creation event: {}", event);
+        } catch (AmqpException e) {
+            log.error("Failed to publish transaction creation event", e);
+        }
     }
 }
 
